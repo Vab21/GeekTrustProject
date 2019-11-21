172b5eb5d222f457f4d3dc80e2f734b4d62b5729
Vaibhav Jain <vibhu.jain21@gmail.com>
2019-11-22 00:32:11 +0530

    cleaning code

--- src/app/find-falcon/services/geek-service.service.ts
+++ src/app/find-falcon/services/geek-service.service.ts
@@ -2,7 +2,6 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
 import { NavigationExtras, Router } from '@angular/router';
 
 @Injectable({
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -6,16 +6,7 @@ import { Component, OnInit, EventEmitter, Output } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
-
-  @Output() callingReset = new EventEmitter<boolean>();
-
   constructor() { }
-
   ngOnInit() {
   }
-
-  callReset() {
-    this.callingReset.emit(true);
-  }
-
 }
